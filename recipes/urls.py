5808a50808785fddd4c37aa2ab527ea688df5ae3
Henrique Gabriel <henriquepro8@gmail.com>
2023-12-15 03:14:16 -0300

    aula 60 completa

--- recipes/urls.py
+++ recipes/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     
     
     path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    
     
 ]
