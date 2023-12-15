5ff02dd547979fb69b068b832533f04bb153ea17
Henrique Gabriel <henriquepro8@gmail.com>
2023-12-15 02:02:46 -0300

    aula 55 completa

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    # função mágica para mostrar o nome da categoria no index de registros no painel admin
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -16,4 +23,9 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
-    
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+ 
