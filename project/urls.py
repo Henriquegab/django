308c8102095351778366871339349b5839a4d60b
Henrique Gabriel <henriquepro8@gmail.com>
2023-12-16 03:08:51 -0300

    fix

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# type: ignore
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -27,4 +29,4 @@ urlpatterns = [
     path('', include('recipes.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # type: ignore
