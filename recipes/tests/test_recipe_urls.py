354a57ce2d74e34e4e56d9fea7e834a48d4502e8
Henrique Gabriel <henriquepro8@gmail.com>
2023-12-22 00:49:50 -0300

    aula 77 ok

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class RecipeURLsTest(TestCase):
         
     def test_recipe_home_url_is_correct(self):
@@ -14,5 +15,3 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
-
-    
